a036ea84b7490de10e7115668e96eaafebd519aa
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2024-06-14 13:35:06 -0500

    reset button creates a new board

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const DEFAULT_DIMENSION = 16;//remove later
-container = document.querySelector('#container');
-resetBtn = document.querySelector('button');
+
+let resetBtn = document.querySelector('button');
 
 resetBtn.addEventListener('click', () => {
-    container.remove();
+    let new_dimension = prompt('enter new dimension');
+    while (new_dimension <= 0 || new_dimension > 100) {
+        new_dimension = prompt('ERROR: enter a valid dimension');
+    }
     
+    let body = document.querySelector('body'); 
+    let container = document.querySelector('#container'); //might remove
+    body.removeChild(container); 
+
+    let new_container = document.createElement('div');
+    new_container.setAttribute('id', 'container');
+    body.appendChild(new_container);
+    
+    createBoard(new_dimension);
 });
 
 function createRow(itemCount) {
@@ -36,6 +48,7 @@ function createItem() {
 
 //todo place in a function
 function createBoard(dimension) {
+    let container = document.querySelector('#container');
     for (let i = 0; i < dimension; i++){
         boardRow = createRow(dimension);
         container.appendChild(boardRow);

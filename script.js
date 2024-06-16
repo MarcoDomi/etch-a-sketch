ef443582cfab27655e582917c56fd1ff45ca22c6
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2024-06-16 11:42:25 -0500

    create name event handlers for applying color and erasing

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const DEFAULT_DIMENSION = 16;//remove later
 
 let resetBtn = document.querySelector('#newBoard');
+let colorBtn = document.querySelector('#color');
+let eraserBtn = document.querySelector('#eraser')
+let rowItems = null;
+
+
+function applyColor(event) {
+    event.target.style.backgroundColor = getRandomColor();
+}
+
+function applyEraser(event) {
+    event.target.style.backgroundColor = 'white';
+}
 
 resetBtn.addEventListener('click', () => {
     let new_dimension = prompt('enter new dimension');
@@ -21,13 +33,28 @@ resetBtn.addEventListener('click', () => {
     }
 });
 
+colorBtn.addEventListener('click', () => {
+    rowItems.forEach(rowItem => {
+        rowItem.removeEventListener('mouseover', applyEraser);
+        rowItem.addEventListener('mouseover', applyColor);
+    }); 
+});
+
+eraserBtn.addEventListener('click', () => {
+    rowItems.forEach(rowItem => {
+        rowItem.removeEventListener('mouseover', applyColor);
+        rowItem.addEventListener('mouseover', applyEraser);
+    });
+});
+
+
 function createRow(itemCount) {
     row = document.createElement('div');
     row.classList.add('row');
     row.style.display = 'flex';
 
     for (let i = 0; i < itemCount; i++){
-        rowItem = createItem();
+        let rowItem = createItem();
         row.appendChild(rowItem);
     }
 
@@ -44,9 +71,11 @@ function getRandomColor() {
         index = Math.floor(Math.random() * hexCharacters.length);
         colorCode += hexCharacters[index];
     }
-    console.log(colorCode);
+   
     return colorCode;
 }
+
+
 
 function createItem() {
     item = document.createElement('div');
@@ -55,9 +84,7 @@ function createItem() {
     item.style.width = '100%';
     item.style.border = '1px solid black';
 
-    item.addEventListener('mouseover', function(event){
-        event.target.style.backgroundColor = getRandomColor();
-    });
+    item.addEventListener('mouseover', applyColor);
 
     return item
 }
@@ -68,7 +95,8 @@ function createBoard(dimension) {
         boardRow = createRow(dimension);
         container.appendChild(boardRow);
     }
+
+    rowItems = document.querySelectorAll('.item');
 }
 
 createBoard(DEFAULT_DIMENSION);
-

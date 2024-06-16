97c7948be84933665718798328240029b3d58647
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2024-06-16 17:20:37 -0500

    increase opacity for every interaction

--- script.js
+++ script.js
@@ -5,9 +5,34 @@ let colorBtn = document.querySelector('#color');
 let eraserBtn = document.querySelector('#eraser')
 let rowItems = null;
 
+function getRandomColor() {
+    const hexCharacters = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+    const colorCodeLength = 6;
+
+    let colorCode = '#';
+
+    for (let i = 0; i < colorCodeLength; i++){
+        index = Math.floor(Math.random() * hexCharacters.length);
+        colorCode += hexCharacters[index];
+    }
+   
+    return colorCode;
+}
+
+function updateOpacity(event) {
+    let currentOpacity = Number(event.target.style.opacity);
+    if (currentOpacity < 10) {
+        console.log('hello');
+        let newOpacity = currentOpacity + 0.1;
+        event.target.style.opacity = newOpacity;
+        console.log(event.target.style.opacity);
+    }
+}
+
 
 function applyColor(event) {
     event.target.style.backgroundColor = getRandomColor();
+    updateOpacity(event);
 }
 
 function applyEraser(event) {
@@ -47,6 +72,18 @@ eraserBtn.addEventListener('click', () => {
     });
 });
 
+function createItem() {
+    item = document.createElement('div');
+    item.classList.add('item');
+    item.style.height = '20px';
+    item.style.width = '100%';
+    item.style.border = '1px solid black';
+    item.style.opacity = '0%';
+    item.addEventListener('mouseover', applyColor);
+
+    return item
+}
+
 
 function createRow(itemCount) {
     row = document.createElement('div');
@@ -61,33 +98,6 @@ function createRow(itemCount) {
     return row
 }
 
-function getRandomColor() {
-    const hexCharacters = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
-    const colorCodeLength = 6;
-
-    let colorCode = '#';
-
-    for (let i = 0; i < colorCodeLength; i++){
-        index = Math.floor(Math.random() * hexCharacters.length);
-        colorCode += hexCharacters[index];
-    }
-   
-    return colorCode;
-}
-
-
-
-function createItem() {
-    item = document.createElement('div');
-    item.classList.add('item');
-    item.style.height = '20px';
-    item.style.width = '100%';
-    item.style.border = '1px solid black';
-
-    item.addEventListener('mouseover', applyColor);
-
-    return item
-}
 
 function createBoard(dimension) {
     let container = document.querySelector('#container');
